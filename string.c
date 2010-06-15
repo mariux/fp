320cca97cb9a2f9cd8cbca15adb501b1a7dc7dd3
matthiasruester <matthiasruester@155de35b-6303-4bdd-9920-02178266c429>
2010-06-15 17:43:25 +0000

    comments in english
    git-svn-id: https://formelparser.svn.sourceforge.net/svnroot/formelparser/trunk@13 155de35b-6303-4bdd-9920-02178266c429

--- string.c
+++ string.c
@@ -16,9 +16,10 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <stdlib.h> /* fuer malloc/free */
-#include <stdio.h>  /* fuer perror/printf */
-#include <string.h> /* fuer strlen */
+
+#include <stdlib.h> /* for malloc/free */
+#include <stdio.h>  /* for perror/printf */
+#include <string.h> /* for strlen */
 
 #include "string.h"
 
