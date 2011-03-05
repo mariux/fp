58ca9073f0b27120ad2d565086b5a918fd7f1605
matthiasruester <matthiasruester@155de35b-6303-4bdd-9920-02178266c429>
2011-03-05 22:07:42 +0000

    turned program name to german again
    git-svn-id: https://formelparser.svn.sourceforge.net/svnroot/formelparser/trunk@71 155de35b-6303-4bdd-9920-02178266c429

--- node.h
+++ node.h
@@ -1,15 +1,15 @@
 /*
-    formula parser - node.h
+    formelparser - node.h
 
     Copyright (C) 2010 Matthias Ruester <ruester@molgen.mpg.de>
     Copyright (C) 2010 Max Planck Institut for Molecular Genetics
 
-    This program is free software: you can redistribute it and/or modify
+    Formelparser is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    Formelparser is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
